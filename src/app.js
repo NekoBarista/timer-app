53f9c1a44e0249282c09c4894710395e80967049
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-07-08 22:53:50 +1000

    timer wired up and animated

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const startButton = document.querySelector('#start')
 const durationInput = document.querySelector('#minutes')
 const pauseButton = document.querySelector('#pause')
+const circle = document.querySelector('circle')
+
+
+perimeter = circle.getAttribute('r')*2*Math.PI
+circle.setAttribute('stroke-dasharray', perimeter)
+
+let duration;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log("Started")
+    onStart(totalDuration) {
+        duration = totalDuration
     },
 
- onTick() {
-    console.log("tick")
+ onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset', perimeter*timeRemaining / duration - perimeter)
+ 
  },
 
- onComplete(
-   
- )
+ onComplete()
  
  {
     console.log('done!')
